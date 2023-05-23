3c3b212f1c1fcfe55cc02806202f4eee5c3571d2
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-23 16:21:12 +0300

    Setting up post routes

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/post';
 import userRoutes from './routes/users';
+import postRoutes from './routes/posts';
+import { verifyToken } from './middleware/auth.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +42,12 @@ const upload = multer({ storage })
 
 // ROUTES WITH FILES
 app.post('/auth/register', upload.single('picture'), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 // MONGOOSE SETUP
 
